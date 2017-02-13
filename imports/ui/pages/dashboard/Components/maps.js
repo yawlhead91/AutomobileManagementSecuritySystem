b0b2e82001a673ac22cf1621ec416c4bee4d6456
yawlhead91 <alexincork91@gmail.com>
2017-02-13 15:05:39 +0000

    Dashboard static

---
+++ imports/ui/pages/dashboard/Components/maps.js
@@ -0,0 +1,30 @@
+import './maps.html';
+
+Template.Map.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('routeMap', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
+
+Template.Map.onRendered(function(){
+	GoogleMaps.load({key: "AIzaSyCOzfISoah8IT6BVNBoq3rdEmboa9Hox98"});
+
+})
+
+Template.Map.helpers({
+  routeMapHelpers: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom: 8
+      };
+    }
+  }
+});
--- imports/ui/pages/dashboard/dashboard.js
+++ imports/ui/pages/dashboard/dashboard.js
@@ -1,1 +1,4 @@
 import './dashboard.html';
+import './Components/sidebar.js'
+import './Components/activitys.js'
+import './Components/maps.js'
